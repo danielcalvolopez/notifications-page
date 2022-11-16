001075aed955d81f841ba3d071f23796a36dc310
Daniel <acidistrict@gmail.com>
2022-11-16 19:03:49 +0000

    adjust styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
         <div className={classes["notifications-header"]}>
           <div className={classes.title}>
             <h2>Notifications</h2>
-            <h3>3</h3>
+            <h3>7</h3>
           </div>
           <h4>Mark all as read</h4>
         </div>
