62c817f347ab62c2462da8fe06c66a5cdbf96a41
danielcalvolopez <acidistrict@gmail.com>
2022-11-17 16:41:21 +0000

    add state to data elements

--- src/data/notifications.js
+++ src/data/notifications.js
@@ -5,12 +5,14 @@ const notifications = [
     description: "reacted to your recent post",
     content: "My first tournament today!",
     time: "1m ago",
+    read: true,
   },
   {
     avatar: "avatars/avatar-angela-gray.webp",
     name: "Angela Gray",
     description: "followed you",
     time: "5m ago",
+    read: true,
   },
   {
     avatar: "avatars/avatar-jacob-thompson.webp",
@@ -18,6 +20,7 @@ const notifications = [
     description: "has joined your group",
     contentclub: "Chess Club",
     time: "1 day ago",
+    read: true,
   },
   {
     avatar: "avatars/avatar-rizky-hasanuddin.webp",
@@ -26,6 +29,7 @@ const notifications = [
     message:
       "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
     time: "5 days ago",
+    read: false,
   },
   {
     avatar: "avatars/avatar-kimberly-smith.webp",
@@ -33,6 +37,7 @@ const notifications = [
     description: "commented on your picture",
     picture: "avatars/image-chess.webp",
     time: "1 week ago",
+    read: false,
   },
   {
     avatar: "avatars/avatar-nathan-peterson.webp",
@@ -40,6 +45,7 @@ const notifications = [
     description: "reacted to your recent post",
     content: "5 end-game strategies to increase your win rate",
     time: "2 weeks ago",
+    read: false,
   },
   {
     avatar: "avatars/avatar-anna-kim.webp",
@@ -47,6 +53,7 @@ const notifications = [
     description: "left the group",
     contentclub: "Chess Club",
     time: "2 weeks ago",
+    read: false,
   },
 ];
 
